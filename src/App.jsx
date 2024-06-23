4cb96621eb14b7948d70988f6f4703945f222a3f
Parashar Das <porashorbabu@gmail.com>
2024-06-23 15:12:28 +0600

    after finish service section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Service from './Components/Service'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Service/>
     </div>
   )
 }
---
+++ src/Components/Service.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Img2 from "../assets/coffee2.png"
+const Service = () => {
+    const ServicesData = [
+        {
+          id: 1,
+          img: Img2,
+          name: "Espresso",
+          description:
+            "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit amet.",
+          aosDelay: "100",
+        },
+        {
+          id: 2,
+          img: Img2,
+          name: "Americano",
+          description:
+            "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit amet",
+          aosDelay: "300",
+        },
+        {
+          id: 3,
+          img: Img2,
+          name: "Cappuccino",
+          description:
+            "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit amet",
+          aosDelay: "500",
+        },
+      ];
+  return (
+    <div className='container py-10'>
+      <h1 className='text-2xl md:text-4xl font-bold capitalize font-cursive text-center text-slate-900'>Best coffee for you </h1>
+      <div className='flex items-center justify-evenly gap-8 pt-[100px]'>
+        {
+            ServicesData.map((item, index)=>(
+                <div className='min-h-[200px] rounded-lg min-w-[300px] relative hover:bg-primary shadow-2xl transition-all duration-200 group py-4' key={index}>
+                    <img src={item.img} className='w-[200px]  group-hover:rotate-[10deg] transition-all duration-300 absolute -top-[100px] left-3 translate-x-9' alt="" />
+                    <div>
+                        <h1 className='text-xl text-center pt-[70px] font-bold'>
+                            {item.name}
+                        </h1>
+                        <p className='max-w-[300px] px-2 pt-4 text-slate-600 text-center line-clamp-2'>
+                            {item.description}
+                        </p>
+                    </div>
+                </div>
+            ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Service
